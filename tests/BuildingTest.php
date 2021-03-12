b2ab9c21564cf42cf2e35dbed93699db0fd9e6de
namelivia <namelivia@users.noreply.github.com>
2021-03-12 16:52:33 +0000

    Apply fixes from StyleCI

--- tests/BuildingTest.php
+++ tests/BuildingTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Namelivia\TravelPerk\Tests;
 
+use Namelivia\TravelPerk\CostCenters\CostCenters\CostCenter;
+use Namelivia\TravelPerk\CostCenters\CostCenters\CostCenters;
 use Namelivia\TravelPerk\Expenses\Invoices\Invoice;
 use Namelivia\TravelPerk\Expenses\Invoices\Invoices;
 use Namelivia\TravelPerk\SCIM\Users\User as SCIMUser;
@@ -11,16 +13,14 @@ use Namelivia\TravelPerk\SCIM\Users\Users as SCIMUsers;
 use Namelivia\TravelPerk\TravelSafe\AirlineMeasures\AirlineMeasure;
 use Namelivia\TravelPerk\TravelSafe\Restrictions\Restriction;
 use Namelivia\TravelPerk\TravelSafe\Summary\Summary;
-use Namelivia\TravelPerk\Users\Users\User;
-use Namelivia\TravelPerk\Users\Users\Users;
-use Namelivia\TravelPerk\Trips\Trips\Trip;
-use Namelivia\TravelPerk\Trips\Trips\Trips;
-use Namelivia\TravelPerk\Webhooks\Webhooks\Webhook;
-use Namelivia\TravelPerk\Webhooks\Webhooks\Webhooks;
 use Namelivia\TravelPerk\Trips\Bookings\Booking;
 use Namelivia\TravelPerk\Trips\Bookings\Bookings;
-use Namelivia\TravelPerk\CostCenters\CostCenters\CostCenter;
-use Namelivia\TravelPerk\CostCenters\CostCenters\CostCenters;
+use Namelivia\TravelPerk\Trips\Trips\Trip;
+use Namelivia\TravelPerk\Trips\Trips\Trips;
+use Namelivia\TravelPerk\Users\Users\User;
+use Namelivia\TravelPerk\Users\Users\Users;
+use Namelivia\TravelPerk\Webhooks\Webhooks\Webhook;
+use Namelivia\TravelPerk\Webhooks\Webhooks\Webhooks;
 
 class BuildingTest extends TestCase
 {
